f95cabbb8b3ff2f262fe4039a512c0d0b5a01da5
meteopavel <isu@meteopavel.ru>
2025-11-23 17:12:12 +0800

    fix: move admin URL to top to prevent slug conflict; secure admin on prod

--- edu_multisite/urls.py
+++ edu_multisite/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = []
+if settings.DEBUG:
+    urlpatterns += [path('admin/', admin.site.urls)]
+
+urlpatterns += [
     path('', include('content.urls')),
 ]
 
