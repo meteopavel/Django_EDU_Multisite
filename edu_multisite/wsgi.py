e6dfbc64100f3b40046b679a57256395c50c37c4
meteopavel <isu@meteopavel.ru>
2025-12-15 15:44:06 +0800

    Add compress

--- edu_multisite/wsgi.py
+++ edu_multisite/wsgi.py
@@ -2,6 +2,18 @@ import os
 import sys
 import platform
 
+# Путь к вашему виртуальному окружению
+VENV_PATH = '/home/c/cj82062/DjangoVOA/django'
+
+# Активируем виртуальное окружение
+activate_this = os.path.join(VENV_PATH, 'bin', 'activate_this.py')
+if os.path.exists(activate_this):
+    exec(open(activate_this).read(), {'__file__': activate_this})
+else:
+    # Если activate_this.py нет (в новых версиях), просто добавляем путь
+    sys.path.insert(0, os.path.join(VENV_PATH, 'lib', 'python3.10', 'site-packages'))
+
+
 sys.path.insert(0, '/home/c/cj82062/DjangoVOA/public_html')
 sys.path.insert(0, '/home/c/cj82062/DjangoVOA/public_html/edu_multisite')
 python_version = ".".join(platform.python_version_tuple()[:2])
