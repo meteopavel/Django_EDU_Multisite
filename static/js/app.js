439e764f599018d19cd9e7fa1fde0534d3325e6a
meteopavel <isu@meteopavel.ru>
2026-03-14 18:43:02 +0800

    Refactor JS (fix entrypoint)

--- static/js/app.js
+++ static/js/app.js
@@ -6,19 +6,9 @@ import { initModal } from './components/modal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initModal();
-    if (document.querySelector('[data-accordion-group="education-accordion"]')) {
-        initEducation();
-    }
-    if (document.querySelector('[data-accordion-group="material-cards"]')) {
-        initMaterials();
-    }
-    if (document.getElementById('news-content-container')) {
-        initNews();
-    }
-    if (document.getElementById('partners-content-container')) {
-        initPartners();
-    }
-    if (document.getElementById('contacts__map') || document.querySelector('.service-card-parking')) {
-        initMaps();
-    }
+    if (document.querySelector('[data-accordion-group="education-accordion"]')) {initEducation();}
+    if (document.querySelector('[data-accordion-group="material-cards"]')) {initMaterials();}
+    if (document.getElementById('news-content-container')) {initNews();}
+    if (document.getElementById('partners-content-container')) {initPartners();}
+    if (document.getElementById('contacts__map') || document.querySelector('.service-card-parking')) {initMaps();}
 });
