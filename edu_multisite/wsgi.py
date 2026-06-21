58d4c85ac0dc3618fb163139fa14fd987f672d43
meteopavel <isu@meteopavel.ru>
2026-06-21 14:49:31 +0800

    wsgi: replace hardcoded hosting paths with HOSTING_ROOT env variable

--- edu_multisite/wsgi.py
+++ edu_multisite/wsgi.py
@@ -5,6 +5,9 @@ WSGI-конфигурация проекта edu_multisite.
 - подготавливает sys.path для окружения хостинга;
 - загружает переменные окружения из .env;
 - инициализирует WSGI-приложение Django.
+
+Переменная окружения HOSTING_ROOT задаёт корень проекта на хостинге.
+Если не задана, используется расположение этого файла.
 """
 
 import os
@@ -12,16 +15,21 @@ import sys
 import platform
 
 
-sys.path.insert(0, '/home/c/cj82062/DjangoVOA/public_html')
-sys.path.insert(0, '/home/c/cj82062/DjangoVOA/public_html/edu_multisite')
+HOSTING_ROOT = os.environ.get(
+    'HOSTING_ROOT',
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 python_version = ".".join(platform.python_version_tuple()[:2])
 
-sys.path.insert(0, '/home/c/cj82062/DjangoVOA//django/lib/python{0}/site-packages'.format(python_version))
+sys.path.insert(0, HOSTING_ROOT)
+sys.path.insert(0, os.path.join(HOSTING_ROOT, 'edu_multisite'))
+sys.path.insert(0, os.path.join(HOSTING_ROOT, '..', 'django', 'lib',
+                                'python{0}'.format(python_version), 'site-packages'))
 os.environ["DJANGO_SETTINGS_MODULE"] = "edu_multisite.settings"
 
 from dotenv import load_dotenv
 
-load_dotenv('/home/c/cj82062/DjangoVOA/.env')
+load_dotenv(os.path.join(HOSTING_ROOT, '..', '.env'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
